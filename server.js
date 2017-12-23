d843824fdd084a523197c17556d263eea8fc10a3
Manish <makkanimation@gmail.com>
2017-12-23 22:15:20 +0530

    Hello World

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-console.log("Hello World!");
+console.log("Hello World!"); 
 const app = express();
 app.get('/',(req,res)=>res.send('Hello World!'));
 app.listen(3000,()=> console.log('Example app listening on port 3000!'));
