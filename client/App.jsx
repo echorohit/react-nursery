bdf28b1455137b75aef02ae5dabf898383097e29
Manish <makkanimation@gmail.com>
2017-12-26 16:25:04 +0530

    Refine Chnages

--- client/App.jsx
+++ client/App.jsx
@@ -2,28 +2,19 @@ import React from 'react';
 import Header from './components/Header.jsx';
 import Sidebar from './components/Sidebar.jsx';
 import MainContent from './components/MainContent.jsx';
-const divStyle = {
-  color: 'blue',
-  textAlign: 'center',
-};
 
 export default class App extends React.Component {
   render() {
   return (
 	<div>
-     <div style={divStyle}>
-        <h1>Hello World</h1>
-      </div>
-    <Header/> 
-    <div className="container-fluid">
-      <div className="row">
-        <Sidebar/>
-        <MainContent/>
-      </div>
+		<Header/> 
+		<div className="container-fluid">
+		  <div className="row">
+			<Sidebar/>
+			<MainContent/>
+		  </div>
+		</div>
     </div>
-    </div>
-	)
-  
-  ;
+	);
   }
 }
