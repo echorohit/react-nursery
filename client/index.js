27338b5019936d5cb916a6b82585b298a5d2b215
neerajnrs <neeraj.nrs104@gmail.com>
2017-12-27 17:58:03 +0530

    commint local file

--- client/index.js
+++ client/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import App from './components/App.jsx';
-=======
 import App from './App.jsx';
 
->>>>>>> master
 ReactDOM.render(<App />, document.getElementById('root'));
