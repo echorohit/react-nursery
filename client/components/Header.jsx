d170cd2f5c5f6e34e8482357e81361a5a576f867
Manish <makkanimation@gmail.com>
2017-12-27 07:40:39 +0530

    Changes Completed

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import HeaderNav from './subcomponents/HeaderNav.jsx';
 export default class Header extends React.Component {
     render(){
         return (
@@ -9,20 +10,7 @@ export default class Header extends React.Component {
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarsExampleDefault">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                        </li>
-                        <li className="nav-item">
-                        <a className="nav-link" href="#">Settings</a>
-                        </li>
-                        <li className="nav-item">
-                        <a className="nav-link" href="#">Profile</a>
-                        </li>
-                        <li className="nav-item">
-                        <a className="nav-link" href="#">Help</a>
-                        </li>
-                    </ul>
+                       <HeaderNav/> 
                     <form className="form-inline mt-2 mt-md-0">
                         <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
                         <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
--- client/components/Sidebar.jsx
+++ client/components/Sidebar.jsx
@@ -1,62 +1,11 @@
 import React from 'react'; 
-const sidebar={
-    position: 'fixed',
-    top: '51px',
-    bottom: '0',
-    left: '0',
-    zIndex: '1000',
-    padding: '20px 0',
-    overflowX: 'hidden',
-    overflowY: 'auto', /* Scrollable contents if viewport is shorter than content. */
-    borderRight: '1px solid #eee',
-}
+import SidebarNav from './subcomponents/SidebarNav.jsx';
+
 
 export default class Sidebar extends React.Component {
     render(){
         return (
-            <nav className="col-sm-3 col-md-2 d-none d-sm-block bg-light" style={sidebar}>
-                <ul className="nav nav-pills flex-column">
-                    <li className="nav-item">
-                    <a className="nav-link active" href="#">Overview <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Reports</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Analytics</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Export</a>
-                    </li>
-                </ul>
-
-                <ul className="nav nav-pills flex-column">
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Nav item</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Nav item again</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">One more nav</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Another nav item</a>
-                    </li>
-                </ul>
-
-                <ul className="nav nav-pills flex-column">
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Nav item again</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">One more nav</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Another nav item</a>
-                    </li>
-                </ul>
-            </nav>
+            <SidebarNav/>
         );
     }
 }
